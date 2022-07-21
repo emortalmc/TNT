f42abf9dc9f8c64ed7048de8baa3fb5e6b6d7009
emortal <nathanpenwill88@gmail.com>
2022-07-21 16:11:08 +0100

    Create TNTExtension.kt

---
+++ src/main/kotlin/dev/emortal/tnt/TNTExtension.kt
@@ -0,0 +1,13 @@
+package dev.emortal.tnt
+
+import net.minestom.server.extensions.Extension
+
+class TNTExtension : Extension() {
+    override fun initialize() {
+
+    }
+
+    override fun terminate() {
+        
+    }
+}
