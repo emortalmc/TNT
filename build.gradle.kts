1ce5d438fd8fb1cec3fa40cff2f4137a0bc4a4b1
Zak Shearman <34372536+ZakShearman@users.noreply.github.com>
2023-05-06 04:05:25 +0100

    Publish to repo.emortal.dev

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,11 @@
 
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
-import org.gradle.internal.impldep.junit.runner.Version.id
 
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.2"
-
-    `maven-publish`
     java
+    `maven-publish`
+
+    id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 repositories {
@@ -21,10 +20,12 @@ dependencies {
 
 //    compileOnly("com.github.hollow-cube:Minestom:e6d4a2cc91")
 //    implementation("com.github.hollow-cube:Minestom:a7440639c8")
-    implementation("com.github.Minestom:Minestom:8ad2c7701f")
-    implementation("ch.qos.logback:logback-classic:1.4.6")
+    implementation("com.github.Minestom:Minestom:85febebd09")
 
-    implementation("com.github.luben:zstd-jni:1.5.2-5")
+    implementation("com.github.luben:zstd-jni:1.5.4-2")
+
+    // Only included for tests
+    testImplementation("ch.qos.logback:logback-classic:1.4.6")
 }
 
 tasks {
@@ -49,11 +50,34 @@ java {
 }
 
 publishing {
+
+    repositories {
+        maven {
+            name = "development"
+            url = uri("https://repo.emortal.dev/snapshots")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+        }
+        maven {
+            name = "release"
+            url = uri("https://repo.emortal.dev/releases")
+            credentials {
+                username = System.getenv("MAVEN_USERNAME")
+                password = System.getenv("MAVEN_SECRET")
+            }
+        }
+    }
+
     publications {
         create<MavenPublication>("maven") {
-            groupId = project.properties["group"] as? String?
-            artifactId = project.name
-            version = project.properties["version"] as? String?
+            groupId = project.properties["group"] as String
+            artifactId = project.properties["name"] as String
+
+            val commitHash = System.getenv("COMMIT_HASH_SHORT")
+            val releaseVersion = System.getenv("RELEASE_VERSION")
+            version = commitHash ?: releaseVersion ?: "local"
 
             from(components["java"])
         }
